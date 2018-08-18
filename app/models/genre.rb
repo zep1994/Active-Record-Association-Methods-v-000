02ce335952cb096684e8a709f43066ff837dd277
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-18 20:20:18 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.name
+    self.artists.map {||}
   end
 end
