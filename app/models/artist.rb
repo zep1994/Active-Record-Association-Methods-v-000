206691b39fce3513e48455491a356fa090645865
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-18 20:15:08 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,11 +9,11 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    
+    self.songs.song_count
   end
 
   def genre_count
     #return the number of genres associated with the artist
-    
+    self.songs.genre_count
   end
 end
