69c96dffef85fee9f52bd223ce98c5022a807af0
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-18 20:10:25 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,16 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    
   end
 
   def song_count
     #return the number of songs associated with the artist
+    
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    
   end
 end
